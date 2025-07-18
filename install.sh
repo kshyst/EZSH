9f86582a0469925b956604ef217733ebd958063e
Kiarash Shojaei Arani <71709479+kshyst@users.noreply.github.com>
2025-07-18 21:06:05 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,18 +9,27 @@ sudo apt-get install -y zsh git curl
 echo "----Installing Oh My Zsh-----"
 export RUNZSH=no
 export CHSH=no
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH="$HOME/.oh-my-zsh"
+if [ -d "$ZSH" ]; then
+    echo "Oh My Zsh already installed in $ZSH. Skipping installation."
+else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 echo "-----Installing Powerlevel10k-----"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+if [ ! -d "$ZSH/custom/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH/custom/themes/powerlevel10k"
+else
+    echo "Powerlevel10k already installed. Skipping."
+fi
 
 echo "-----Configuring .zshrc for Powerlevel10k------"
-sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' $HOME/.zshrc
-if ! grep -q 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' $HOME/.zshrc; then
-    echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >> $HOME/.zshrc
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$HOME/.zshrc"
+if ! grep -q 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' "$HOME/.zshrc"; then
+    echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >> "$HOME/.zshrc"
 fi
 
 echo "-----Setting zsh as default shell------"
-sudo chsh -s $(which zsh)
+sudo chsh -s "$(which zsh)" "$USER"
 
 echo "Open a new terminal to access zsh ;)"
