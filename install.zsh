535eed742d5d9ed67d94a500b712a9c480356c02
Kiarash Shojaei Arani <71709479+kshyst@users.noreply.github.com>
2025-07-18 20:06:54 +0330

    Create install.zsh

---
+++ install.zsh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "-----Installing zsh-----"
+sudo apt-get update
+sudo apt-get install -y zsh git curl
+
+echo "----Installing Oh My Zsh-----"
+export RUNZSH=no
+export CHSH=no
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "-----Installing Powerlevel10k-----"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+
+echo "-----Configuring .zshrc for Powerlevel10k------"
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' $HOME/.zshrc
+
+echo "-----Setting zsh as default shell------"
+chsh -s $(which zsh)
+
+echo "Open a new terminal to access zsh ;)"
